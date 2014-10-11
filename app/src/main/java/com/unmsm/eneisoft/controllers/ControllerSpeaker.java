694f5f808643aa0ea71ecc541752cd11dd873520
manu <manuelriosvega@gmail.com>
2014-10-10 19:33:34 -0500

    +fragment game jam

--- app/src/main/java/com/unmsm/eneisoft/controllers/ControllerSpeaker.java
+++ app/src/main/java/com/unmsm/eneisoft/controllers/ControllerSpeaker.java
@@ -18,10 +18,8 @@ public class ControllerSpeaker {
         this.data.add(new Speaker("Eysenck Gomez", "Un workflow moderno para crear aplicaciones web modernas.", R.drawable.speaker2));
         this.data.add(new Speaker("Alvaro Pereyra", "¡Cómo ser un desarrollador en una StartUp y no morir en el intento!", R.drawable.speaker3));
         this.data.add(new Speaker("Alfred Kobayashi", "Las top 5 Cosas más importantes para crear un StartUp exitoso.", R.drawable.speaker4));
-        this.data.add(new Speaker("Jesus Andrade", "Talento mundial AIESEC", R.drawable.speaker1));
-        this.data.add(new Speaker("Eysenck Gomez", "Un workflow moderno para crear aplicaciones web modernas.", R.drawable.speaker2));
-        this.data.add(new Speaker("Alvaro Pereyra", "¡Cómo ser un desarrollador en una StartUp y no morir en el intento!", R.drawable.speaker3));
-        this.data.add(new Speaker("Alfred Kobayashi", "Las top 5 Cosas más importantes para crear un StartUp exitoso.", R.drawable.speaker4));
+        this.data.add(new Speaker("Juan Sanchez", "App-ocalipsis: ¡El fin de las aplicaciones móviles!", R.drawable.speaker5));
+        this.data.add(new Speaker("Moises Cachay", "Introduccion a la teoria musical con Python..", R.drawable.speaker6));
 
     }
 
